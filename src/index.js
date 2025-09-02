530eaff241c982820d90a5770f3ca2a54585b68f
kjminn <kjminn2001@gmail.com>
2025-09-02 16:59:28 +0900

    Button, ConfirmDialog,ConfirmDialog, Clock 함수형컴퍼넌트(React Element Rendering)

--- src/index.js
+++ src/index.js
@@ -3,13 +3,26 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import AttendanceCard from "./chp10/AttendanceCard";
+import Library from "./chp03/Library";
+import './chp03/Book.css'
+import ConfirmDialog from "./chp04/ConfirmDialog";
+import './chp04/Button.css'
+import './chp04/ConfirmDialog.css'
+import ConfirmDialogs from "./chp04/ConfirmDialogs";
+import Clock from "./chp04/Clock";
+import './chp04/clock.css'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+
+setInterval(()=>{
+    root.render(
+        <React.StrictMode>
+            <Clock />
+        </React.StrictMode>
+    );
+}, 1000)
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))

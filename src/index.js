057b7b37c247583c488a4949bc6bd5fc724c7d16
kjminn <kjminn2001@gmail.com>
2025-10-28 16:13:50 +0900

    10장 List and Key

--- src/index.js
+++ src/index.js
@@ -3,23 +3,14 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import Toggle from "./chp08/Toggle";
-import Toggle_2 from "./chp08/Toggle_2";
-import Toggle_3_NewSyntax from "./chp08/Toggle_3_NewSyntax";
-import ToggleFunction from "./chp08/ToggleFunction";
-import MyButton_Argument from "./chp08/MyButton_Argument";
-import InputTest from "./chp08/InputTest";
-import ConfirmButtonFunction from "./chp08/ConfirmButtonFunction";
-import Greeting from "./chp09/ex1/Greeting";
-import LoginControl from "./chp09/ex2/LoginControl";
-import MainPageWarning from "./chp09/ex3/MainPageWarning";
-import LandingPage from "./chp09/closing_ex/LandingPage";
+import StudentAttendance from "./chp10/ex2/StudentAttendance";
+import AttendanceCard from "./chp10/ex3/AttendanceCard";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
     <React.StrictMode>
-        <LandingPage/>
+        <AttendanceCard/>
     </React.StrictMode>
 );
 
